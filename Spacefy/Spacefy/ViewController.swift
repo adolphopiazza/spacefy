bd7206ccf0132cbb26aa407b544209d82524edfc
Adolpho Piazza <adolpho@premiersoft.net>
2021-03-07 20:31:27 -0300

    Initial commit, base project.

---
+++ Spacefy/Spacefy/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Spacefy
+//
+//  Created by Adolpho Piazza on 07/03/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .cyan
+    }
+
+}
+
