734a153e74c3485c612cee6ade3c9ca3581bc209
adolphopiazza <adolphopiazza@gmail.com>
2021-04-01 11:22:21 -0300

    [ADD] New view controller: Launches [ADD] New tab bar item for Launches VC

---
+++ Spacefy/Spacefy/Scenes/Launches/LaunchesViewController.swift
@@ -0,0 +1,19 @@
+//
+//  LaunchesViewController.swift
+//  Spacefy
+//
+//  Created by Adolpho Piazza on 01/04/21.
+//
+
+import UIKit
+
+class LaunchesViewController: SFYBaseViewController {
+
+    static let launchesVCTitle = "Launches"
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        title = LaunchesViewController.launchesVCTitle
+    }
+
+}
